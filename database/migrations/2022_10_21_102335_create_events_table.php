9d72b8fc1005ce08d450d93734cece3eba98b975
Nelson <juniormoncada17@gmail.com>
2022-10-21 13:45:55 +0200

    create databases

---
+++ database/migrations/2022_10_21_102335_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('descripcion');
+            $table->string('localizacion');
+            $table->boolean('verified');
+            $table->string('organizador');
+            $table->integer('personas_inscritas');
+            $table->string('img');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
