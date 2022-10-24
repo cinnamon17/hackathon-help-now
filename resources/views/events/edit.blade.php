83d5601e3126e4db73bbb560eaacc061863c77a8
Nelson <juniormoncada17@gmail.com>
2022-10-24 19:17:54 +0200

    dashboard view

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -1,10 +1,15 @@
-<form action="{{route('event.update', $event) }}" method="POST">
+<form id="event-edit-form" action="{{route('event.update', $event) }}" method="POST">
 
 @csrf
 @method('PUT')
-
-<input type="text" name="title" value="{{ $event->title}}">
-<input type="text" name="descripcion" value="{{ $event->descripcion}}">
+<label for="title">Titulo</label>
+<input id="title" type="text" name="title" value="{{ $event->title}}">
+<label for="descripcion">Descripción</label>
+<input id="descripcion" type="text" name="descripcion" value="{{ $event->descripcion}}">
+<label for="img">Imagen</label>
+<input id="img" type="text" name="img" value="{{ $event->img}}">
+<label for="localizacion">Localización</label>
+<input id="localizacion" type="text" name="localizacion" value="{{ $event->localizacion}}">
 
 <button type="submit">Update</button>
 
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,10 +1,19 @@
 <div>
+<form action="{{route('event.edit', $event)}}" method="POST">
+
+@csrf
+@method('GET')
 
 {{ $event->title }}
 {{ $event->descripcion }}
 
-</div>
+<button type="submit">Edit</button>
+</form>
 <div>
 
 
 </div>
+
+</div>
+
+
